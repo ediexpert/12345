4b7964a7b9008b415809e55aafb4286d60b5e878
Spare <spare@Sara-Mac-Pro.local>
2016-06-22 00:47:35 +0400

    background username password intraction and keyboard hide and buttton corener and lock logo added

--- NewVIPAiOS/ViewController.h
+++ NewVIPAiOS/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *bgWebView;
+@property (weak, nonatomic) IBOutlet UIImageView *logo;
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UILabel *registerButton;
+- (IBAction)loginButtonAction:(id)sender;
+@property (weak, nonatomic) IBOutlet UIWebView *background;
 
 @end
--- NewVIPAiOS/ViewController.m
+++ NewVIPAiOS/ViewController.m
@@ -7,17 +7,33 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize bgWebView;
+@synthesize loginButton;
+@synthesize background;
+@synthesize username;
+@synthesize password;
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad]; 
+
+	NSString *filePath = [[NSBundle mainBundle] pathForResource:@"vipaiOS" ofType:@"gif"];
+    NSData *gif = [NSData dataWithContentsOfFile:filePath];
+//    UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width , [[UIScreen mainScreen] bounds].size.height)];
+    
+//    webview.userInteractionEnabled = NO;
+//    [webview loadData:gif MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
+//    [self.view addSubview:webview];
+    [background loadData:gif MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
+    loginButton.layer.cornerRadius=9;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +42,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)loginButtonAction:(id)sender {
+    [self.username resignFirstResponder];
+    [self.password resignFirstResponder];
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
 @end
